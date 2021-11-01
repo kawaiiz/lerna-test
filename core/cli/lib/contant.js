d948f588624dbf224846be38a040a45922cd5122
zml <zmlq7@outlook.com>
2021-11-01 18:31:22 +0800

    第一课结束

--- core/cli/lib/contant.js
+++ core/cli/lib/contant.js
@@ -1,6 +1,8 @@
-
-const LOWEST_NODE_VERSION = "v12.0.0"
+// cli/contant.js
+const LOWEST_NODE_VERSION = "v12.0.0";
+const DEFAULT_CLI_HOME = "./.zml-lerna-test";
 
 module.exports = {
-  LOWEST_NODE_VERSION
-}
+  LOWEST_NODE_VERSION,
+  DEFAULT_CLI_HOME,
+};
